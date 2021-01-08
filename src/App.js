84897885b85928adef47eec53909c17ff1a2b5eb
nelo <nelokwaz@gmail.com>
2021-01-08 14:09:00 +0100

    added the base structure of the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Unsplash Image Gallery!</h1>
+
+      <form>
+        <input type="text" placeholder="Search Unsplash..." />
+        <button>Search</button>
+      </form>
+
+      <div className="image-grid">
+        {[...Array(100)].map((_, index) => (
+          <div className="image" key={index}>
+            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
