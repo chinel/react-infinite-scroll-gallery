29cd21efcfbec5192caf3e4d80090908d3e8dbd8
nelo <nelokwaz@gmail.com>
2021-01-12 06:37:22 +0100

    added the infinite library in the app component and wrapped it around the image grid div

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./App.css";
 
 const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
@@ -30,14 +31,35 @@ export default function App() {
         <input type="text" placeholder="Search Unsplash..." />
         <button>Search</button>
       </form>
-
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.regular} alt={image.alt_description} />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={items.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        // below props only if you need pull down functionality
+        refreshFunction={this.refresh}
+        pullDownToRefresh
+        pullDownToRefreshThreshold={50}
+        pullDownToRefreshContent={
+          <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
+        }
+        releaseToRefreshContent={
+          <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
+        }
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <div className="image" key={index}>
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
