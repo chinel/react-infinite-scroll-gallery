766c3d4b296bdc19fe9d58fe9bab4a4e643ac233
nelo <nelokwaz@gmail.com>
2021-01-12 08:04:31 +0100

    wrapped the images with an anchor element

--- src/App.js
+++ src/App.js
@@ -32,31 +32,22 @@ export default function App() {
         <button>Search</button>
       </form>
       <InfiniteScroll
-        dataLength={items.length} //This is important field to render the next data
+        dataLength={images.length} //This is important field to render the next data
         next={fetchData}
         hasMore={true}
         loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-        // below props only if you need pull down functionality
-        refreshFunction={this.refresh}
-        pullDownToRefresh
-        pullDownToRefreshThreshold={50}
-        pullDownToRefreshContent={
-          <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
-        }
-        releaseToRefreshContent={
-          <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
-        }
       >
         <div className="image-grid">
           {images.map((image, index) => (
-            <div className="image" key={index}>
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={image.urls.regular} alt={image.alt_description} />
-            </div>
+            </a>
           ))}
         </div>
       </InfiniteScroll>
